c59bfc1964bc207ee526e66ccd7ebcad96b4b555
jxes993409 <jxes993409@gmail.com>
2023-11-28 20:35:41 +0800

    Add file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import camelot
-
+import jieba
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class pdf2text:
     def __init__(self):
@@ -9,35 +11,52 @@ class pdf2text:
         tables = camelot.read_pdf(pdf_file, pages="all")
         texts = []
         for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
+            data = table.data
+            data = [' '.join(row) for row in data]
+            text = ' '.join(data)
+            text = text.replace('\n', '')
+            texts.append(text)
+        return texts
 
 
 class text2vector:
     def __init__(self):
-        pass
+        self.vectors = TfidfVectorizer()
 
     def __call__(self, text):
-        pass
+        return self.vectors.fit_transform(text)
 
 
 class cosine_sim:
     def __init__(self):
         pass
 
-    def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+    def __call__(self, vector_from_keyword, vector_from_table):
+        return cosine_similarity(vector_from_keyword, vector_from_table)
 
 
 def main(keyword, pdf_file):
     pdf_parser = pdf2text()
+    text_vector = text2vector()
+    similarity = cosine_sim()
+
     table_text = pdf_parser(pdf_file)
-    print(table_text)
+
+    keyword_split = jieba.lcut(keyword)
+    table_vectors = text_vector(table_text + keyword_split)
+    keyword_vectors = text_vector(keyword_split)
+    
+    print(table_vectors.shape)
+
+    # for vector in table_vectors:
+    #     results = []
+    #     result = similarity(vector, table_vectors[2])
+    #     results.append(result)
+    #     print(results)
     # return table
 
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+
+    main("非監督式學習應用", "/mnt/r12921068/DS920/BDS/HW3a/docs/1.pdf")
+    # main("keyword", "/mnt/r12921068/DS920/BDS/HW3a/docs/2.pdf")
