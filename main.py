b208d8247bd8e55a60077cc497d83f52ce255a07
ice025atline <113770349+ice025atline@users.noreply.github.com>
2023-12-02 09:03:37 +0800

    main.py

--- main.py
+++ main.py
@@ -1,43 +1,75 @@
 import camelot
+import numpy as np
+import pandas as pd
+from numpy.linalg import norm
+import streamlit as st
+from sentence_transformers import SentenceTransformer
 
-
-class pdf2text:
+class TableExtractor:
     def __init__(self):
-        pass
+        self.tables_data = []
 
-    def __call__(self, pdf_file):
-        tables = camelot.read_pdf(pdf_file, pages="all")
-        texts = []
+    def extract_tables(self, pdf_file_path: str):
+        # Use Camelot to parse tables in the PDF
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
+        print("Total tables extracted:", tables.n)
         for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
+            # Convert table data to a dictionary, including DataFrame and text representation of the table
+            self.tables_data.append({
+                "df": table.df,
+                "text": table.df.to_string().replace("\\n", "")
+            })
+        return self.tables_data
 
-
-class text2vector:
+class Vectorizer:
     def __init__(self):
-        pass
+        self.model = SentenceTransformer('shibing624/text2vec-base-chinese')
 
-    def __call__(self, text):
-        pass
+    def encode(self, text):
+        # Use the model to convert text to vectors
+        return self.model.encode(text, convert_to_tensor=False)
 
-
-class cosine_sim:
-    def __init__(self):
-        pass
-
+class CosineSimilarityCalculator:
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        # Calculate cosine similarity between two vectors
+        cosine_similarity = np.dot(vector_from_table, vector_from_keyword) / (norm(vector_from_table) * norm(vector_from_keyword))
+        return cosine_similarity
 
+def search_best_table(keyword, pdf_file_path) -> pd.DataFrame:
+    table_extractor = TableExtractor()
+    # Extract tables from the PDF
+    tables_data = table_extractor.extract_tables(pdf_file_path)
+    
+    vectorizer = Vectorizer()
+    # Convert table text to vectors
+    vectors = [vectorizer.encode(table["text"]) for table in tables_data]
+    
+    cosine_similarity_calculator = CosineSimilarityCalculator()
+    # Calculate cosine similarity
+    cosine_scores = [cosine_similarity_calculator(vector, vectorizer.encode(keyword)) for vector in vectors]
 
-def main(keyword, pdf_file):
-    pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    return tables_data[np.argmax(cosine_scores)]["df"]
 
+def UI():
+    st.title('PDF Table Search Engine')
+    st.write("Please upload your PDF file")
+    upload_file = st.file_uploader("Upload YOUR PDF File", type=['pdf'])
+
+    if upload_file is not None:
+        st.write("You uploaded the file")
+        st.write(upload_file.name)
+        pdf_file = f"docs/{upload_file.name}"
+        with open(pdf_file, 'wb') as f:
+            f.write(upload_file.read())
+
+    st.write("Please enter the keyword you want to search for")
+    keyword = st.text_input("Keyword", "Application of Unsupervised Learning")
+
+    if st.button("Search"):
+        # Call the search function and display the result
+        table = search_best_table(keyword, pdf_file)
+        st.write("Result")
+        st.dataframe(table)
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    UI()
