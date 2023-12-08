55019030f7a062972af7f96268af91d69ba6c047
Muhammad Aulia Rahman <muhammadauliarahman@MacBook-Pro.local>
2023-12-08 20:05:42 +0800

    R12H45006

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import camelot
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class pdf2text:
     def __init__(self):
@@ -14,30 +15,57 @@ class pdf2text:
         text = text.replace("\\n", "")
         return text
 
-
 class text2vector:
     def __init__(self):
-        pass
+        self.vectorizer = TfidfVectorizer()
 
     def __call__(self, text):
-        pass
-
+        return self.vectorizer.fit_transform([text])
 
 class cosine_sim:
     def __init__(self):
         pass
 
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        return cosine_similarity(vector_from_table, vector_from_keyword)
 
+def search_table_with_keyword(keyword, pdf_file):
+    pdf_parser = pdf2text()
+    text_converter = text2vector()
+    cosine_calculator = cosine_sim()
+
+    table_text = pdf_parser(pdf_file)
+    keyword_vector = text_converter(keyword)
+    table_vector = text_converter(table_text)
+
+    similarity_score = cosine_calculator(table_vector, keyword_vector)
+    
+    # Assuming a threshold for similarity (you may adjust this based on your needs)
+    similarity_threshold = 0.5
+
+    if similarity_score[0][0] > similarity_threshold:
+        tables = camelot.read_pdf(pdf_file, pages="all")
+        matching_tables = []
+        for i, table in enumerate(tables):
+            table_text = table.df.to_string()
+            table_vector = text_converter(table_text)
+            similarity_score_table = cosine_calculator(table_vector, keyword_vector)
+            if similarity_score_table[0][0] > similarity_threshold:
+                matching_tables.append(table.df)
+        return matching_tables
+    else:
+        return None
 
 def main(keyword, pdf_file):
-    pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    matching_tables = search_table_with_keyword(keyword, pdf_file)
 
+    if matching_tables:
+        print(f"Found in '{pdf_file}':")
+        for i, table in enumerate(matching_tables):
+            print(f"Table {i + 1}:\n{table}\n")
+    else:
+        print(f"No matching table found in '{pdf_file}' for keyword '{keyword}'.")
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    main("desired information", "docs/1.pdf")
+    main("desired information", "docs/2.pdf")
