cca956cc70697ff315852daf4cf473b1461a1f75
Rex-Shih <pipirex77@gmail.com>
2023-12-09 02:13:37 +0800

    fst

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import camelot
-
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
 
 class pdf2text:
-    def __init__(self):
-        pass
-
     def __call__(self, pdf_file):
         tables = camelot.read_pdf(pdf_file, pages="all")
         texts = []
@@ -12,32 +12,46 @@ class pdf2text:
             texts.append(table.df.to_string())
         text = "\n".join(texts)
         text = text.replace("\\n", "")
-        return text
-
+        return texts  # Return list of texts for each table
 
 class text2vector:
     def __init__(self):
-        pass
+        self.vectorizer = TfidfVectorizer()
 
-    def __call__(self, text):
-        pass
-
+    def __call__(self, texts):
+        return self.vectorizer.fit_transform(texts)
 
 class cosine_sim:
-    def __init__(self):
-        pass
-
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        return cosine_similarity(vector_from_table, vector_from_keyword)
 
 
-def main(keyword, pdf_file):
+def analyze_pdf(keyword, pdf_file):
     pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    tables_text = pdf_parser(pdf_file)
+    
+    vectorizer = text2vector()
+    tables_vector = vectorizer(tables_text)
+    
+    keyword_vector = vectorizer.vectorizer.transform([keyword])
+    
+    similarity = cosine_sim()
+    cos_sim_scores = similarity(tables_vector, keyword_vector)
+    
+    most_similar_table_index = np.argmax(cos_sim_scores)
+    return tables_text[most_similar_table_index]
 
+# Streamlit interface
+st.title("PDF Table Search Tool")
 
-if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+# User input for the search keyword
+keyword = st.text_input("Enter the search keyword:")
+
+# File uploader allows user to add their own PDF
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+# When the user uploads a file and enters a keyword, process the PDF
+if uploaded_file is not None and keyword:
+    with st.spinner('Analyzing...'):
+        result = analyze_pdf(keyword, uploaded_file)
+        st.write(result)
