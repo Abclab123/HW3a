b49d0550ba1a21ea33941f0735023e24e8df76a1
Rex-Shih <pipirex@gmail.com>
2023-12-09 10:41:33 +0800

    B08902051

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import camelot
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer, util
+
 import streamlit as st
 import tempfile
 
@@ -14,38 +16,41 @@ class pdf2text:
         tables = camelot.read_pdf(tmpfile_path, pages="all")
         texts = []
         for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
+            text = table.df.to_string()
+            text = text.replace('\n', '')
+            texts.append({
+                "df": table.df,
+                "string": text,
+            })
         return texts  # Return list of texts for each table
 
 class text2vector:
     def __init__(self):
-        self.vectorizer = TfidfVectorizer()
+        self.model = SentenceTransformer("thenlper/gte-large")
 
     def __call__(self, texts):
-        return self.vectorizer.fit_transform(texts)
+        return self.model.encode(texts)
 
 class cosine_sim:
     def __call__(self, vector_from_table, vector_from_keyword):
-        return cosine_similarity(vector_from_table, vector_from_keyword)
+        return util.cos_sim(vector_from_table, vector_from_keyword)
 
 
 def analyze_pdf(keyword, pdf_file):
     pdf_parser = pdf2text()
-    #st.write(pdf_file)
-    tables_text = pdf_parser(pdf_file)
+    tables = pdf_parser(pdf_file)
     
     vectorizer = text2vector()
+    
+    tables_text = [i["string"] for i in tables]
+    
     tables_vector = vectorizer(tables_text)
+    keyword_vector = vectorizer(keyword)
+    test_sim = cosine_sim()
+    similarity = test_sim(keyword_vector, tables_vector)
+    highest = np.argmax(similarity, axis=-1)
     
-    keyword_vector = vectorizer.vectorizer.transform([keyword])
-    
-    similarity = cosine_sim()
-    cos_sim_scores = similarity(tables_vector, keyword_vector)
-    
-    most_similar_table_index = np.argmax(cos_sim_scores)
-    return tables_text[most_similar_table_index]
+    return tables[highest[0].item()]["df"]
 
 # Streamlit interface
 st.title("PDF Table Search Tool")
